ecc1225d70aab5dffb45a19d4b78214bbdfee383
Ahmed-617 <ahmed.benamor1@esprit.tn>
2022-02-19 10:30:19 +0100

    Initial commit

---
+++ src/main/java/tn/microfinance/fincro/FincroApplication.java
@@ -0,0 +1,13 @@
+package tn.microfinance.fincro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/microfinance/fincro/FincroApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.microfinance.fincro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
