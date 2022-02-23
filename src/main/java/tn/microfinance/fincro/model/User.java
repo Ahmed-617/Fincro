7a7f64fc62d061ea5747cd22b7ae0283cd08014b
belki <ayari.balkis@esprit.tn>
2022-02-23 11:20:48 +0100

    ahmed

--- src/main/java/tn/microfinance/fincro/model/User.java
+++ src/main/java/tn/microfinance/fincro/model/User.java
@@ -5,11 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @NoArgsConstructor
 
 @AllArgsConstructor
+@Entity
+
 public class User {
+    @Id
+    private int idUser;
+
 
 }
