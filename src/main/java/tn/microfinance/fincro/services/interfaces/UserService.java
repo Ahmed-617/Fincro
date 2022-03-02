1656bb7263a5af6d34be4db5827b2ee54a7610b2
belki <ayari.balkis@esprit.tn>
2022-03-02 10:58:59 +0100

    rep+serv+impl

--- src/main/java/tn/microfinance/fincro/services/interfaces/UserService.java
+++ src/main/java/tn/microfinance/fincro/services/interfaces/UserService.java
@@ -1,4 +1,15 @@
 package tn.microfinance.fincro.services.interfaces;
 
+import org.springframework.stereotype.Service;
+import tn.microfinance.fincro.dao.model.User;
+
+import java.util.List;
+
+@Service
 public interface UserService {
+    List<User>retrieveAllUsers();
+    User addUser(User u);
+    void deleteUser(Integer id);
+    User upadateUser(User u);
+    User retrieveUser(Integer id);
 }
