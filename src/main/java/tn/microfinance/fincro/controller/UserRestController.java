258e5e5c531ed0eae34d9e5d8f047f3b3a08f790
belki <ayari.balkis@esprit.tn>
2022-03-02 11:44:18 +0100

    UserRestController

--- src/main/java/tn/microfinance/fincro/controller/UserRestController.java
+++ src/main/java/tn/microfinance/fincro/controller/UserRestController.java
@@ -1,8 +1,8 @@
 package tn.microfinance.fincro.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tn.microfinance.fincro.dao.model.Transaction;
 import tn.microfinance.fincro.dao.model.User;
 import tn.microfinance.fincro.services.interfaces.UserService;
 
@@ -13,8 +13,30 @@ public class UserRestController {
     @Autowired
     UserService userService;
     @GetMapping("getAllUsers")
-    public List<User>getAllUsers(){
+    public List<User>getAllUsers() {
         return userService.retrieveAllUsers();
     }
+    @GetMapping("getUser/{id}")
+    public User getUserById(@PathVariable("id") Integer id) {
+        return userService.retrieveUser(id);
+    }
+
+    @PostMapping("getUsers")
+    public User addUser(@RequestBody User user){
+    return userService.addUser(user);
+    }
+   @DeleteMapping("deleteUser/{id}")
+    public void deleteUser(@PathVariable("id") Integer id){
+        userService.deleteUser(id);
+   }
 
 }
+
+
+
+
+
+
+
+
+
