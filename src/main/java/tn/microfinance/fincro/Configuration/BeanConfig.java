507aa9712144e924715bb017468b697af580d53b
belki <ayari.balkis@esprit.tn>
2022-04-08 01:22:36 +0200

    final

--- src/main/java/tn/microfinance/fincro/Configuration/BeanConfig.java
+++ src/main/java/tn/microfinance/fincro/Configuration/BeanConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanConfig {
-
+//beanpassword
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
