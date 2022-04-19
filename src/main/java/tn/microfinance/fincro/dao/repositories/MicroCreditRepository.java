ea854c5e1af34038de31da97d2a97e1aa4373e7a
Mehdi Hantous <mehdi.hantous@esprit.tn>
2022-04-19 22:45:55 +0100

    Final commit MicroCredit

--- src/main/java/tn/microfinance/fincro/dao/repositories/MicroCreditRepository.java
+++ src/main/java/tn/microfinance/fincro/dao/repositories/MicroCreditRepository.java
@@ -1,11 +1,21 @@
 package tn.microfinance.fincro.dao.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import tn.microfinance.fincro.dao.model.CreditStatus;
+import tn.microfinance.fincro.dao.model.CreditType;
 import tn.microfinance.fincro.dao.model.MicroCredit;
+
+import java.util.List;
 
 @Repository
 public interface MicroCreditRepository extends CrudRepository<MicroCredit,Long> {
     //public void Simulation(double amount,int period,double interest);
+
+    @Query("SELECT c from  MicroCredit c where c.creditType = ?1")
+    List<MicroCredit> retrieveCreditsByType(CreditType type);
+
+    @Query("SELECT c from  MicroCredit c where c.status = ?1")
+    List<MicroCredit> retrieveCreditsByStatus(CreditStatus status);
 }
-//
--- src/main/java/tn/microfinance/fincro/services/interfaces/MicroCreditService.java
+++ src/main/java/tn/microfinance/fincro/services/interfaces/MicroCreditService.java
@@ -1,5 +1,7 @@
 package tn.microfinance.fincro.services.interfaces;
 
+import tn.microfinance.fincro.dao.model.CreditStatus;
+import tn.microfinance.fincro.dao.model.CreditType;
 import tn.microfinance.fincro.dao.model.MicroCredit;
 
 import java.util.Hashtable;
@@ -8,7 +10,7 @@ import java.util.List;
 public interface MicroCreditService {
     List<MicroCredit> retrieveAllCredits();
 
-    MicroCredit addCredit(MicroCredit c);
+    MicroCredit addCredit(MicroCredit c,Long idAccount);
 
     void archiveCredit(Long id);
 
@@ -18,12 +20,19 @@ public interface MicroCreditService {
 
     MicroCredit retrieveCredit(Long id);
 
+    List<MicroCredit> getCreditsByType(CreditType type);
+
+    List<MicroCredit> getCreditsByStatus(CreditStatus status);
+
     Hashtable<String, Double> Simulation(double amount, int period,String typePeriod);
 
     double score(double amount,int period,String typePeriod);
 
-    Hashtable<String,Double> FailureToPay(long idCredit,double crd,double interestAmount);
+    Hashtable<String,Double> FailureToPay(long idCredit,int period,double interestAmount);
 
     double calculateInterest(double score);
+
+    //Recomendation
+
+    int CapacityToPay(long idCredit, double crd);
 }
-//
